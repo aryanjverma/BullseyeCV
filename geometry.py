20f9bb708ea2ad54e8b130c02932b34d605c3fb5
Aryan Verma <aryanjverma2@gmail.com>
2026-01-06 16:27:29 -0600

    organizes functions, creates better center finding algorithm

---
+++ geometry.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+import math
+
+def findCenters(gray):
+    rows = gray.shape[0]
+    minRadius = 50
+    maxRadius = 150
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, rows / 8,
+                                param1=100, param2=50,
+                                minRadius=minRadius, maxRadius=maxRadius)
+    centers = []
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for circle in circles[0, :]:
+            centers.append((circle[0], circle[1]))
+    return centers
+
+circularityCutoff = 0.75
+areaMin = 10
+areaMax = 700000
+def findCircles(contours):
+    circles = []
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        perimeter = cv2.arcLength(contour, True)
+        if perimeter != 0:
+            circularity = (4 * math.pi * area) / (perimeter**2)
+            if circularity > circularityCutoff and areaMin < area < areaMax:
+                circles.append(cv2.minEnclosingCircle(contour))
+    return circles
+
+distanceCutoff = 20
+radiusCutoff = 10
+def findRadii(centers, circles, maxRadius):
+    centerRadiiMap = dict()
+    for center in centers:
+        cx, cy = center
+        radiiSums = []
+        radiiCounts = []
+        for circle in circles:
+            (x, y), r = circle
+            if abs(cx - x) < distanceCutoff and abs(cy - y) < distanceCutoff and radiusCutoff < r < maxRadius:
+                needToAdd = True
+                for index in range(len(radiiSums)):
+                    if abs(radiiSums[index] / radiiCounts[index] - r) < radiusCutoff:
+                        radiiSums[index] += r
+                        radiiCounts[index] += 1
+                        needToAdd = False
+                        break
+                if needToAdd:
+                    radiiSums.append(r)
+                    radiiCounts.append(1)
+        radii = []
+        for index in range(len(radiiCounts)):
+            radii.append(radiiSums[index] / radiiCounts[index])
+        centerRadiiMap[(cx, cy)] = radii
+    return centerRadiiMap
--- main.py
+++ main.py
@@ -1,95 +1,41 @@
 import cv2
-import math
+from geometry import *
 
-image = cv2.imread('hd.webp')
+image = cv2.imread('5warrows.png')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(
     gray,
     threshold1=80,
     threshold2=160
 )
-edges = cv2.GaussianBlur(edges, (5,5), 0)
+edges = cv2.GaussianBlur(edges, (3,3), 0)
 
-contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-sizeCutoff = 50
-print(hierarchy)
-def contourDepth(hierarchy, index):
-    depth = 0
-    current = index
-    while hierarchy[0][current][3] != -1:
-        depth += 1
-        current = hierarchy[0][current][3]
-    return depth
-def findNDeepest(hierarchy, N):
-    depths = []
-    for index in range(len(hierarchy[0])):
-        if hierarchy[0][index][2] == -1:
-            depth = contourDepth(hierarchy, index)
-            depths.append((depth, index))
-    depths.sort(key=lambda x: x[0], reverse=True)
-    deepestDepths = []
-    for index in range(N):
-        deepestDepths.append(depths[index][1])
-    return deepestDepths
-def findCenter(contour):
-    M = cv2.moments(contour)
-    if M['m00'] != 0:
-        cx = M['m10'] / M['m00']
-        cy = M['m01'] / M['m00']
-        return (cx, cy)
-deepestIndeces = findNDeepest(hierarchy, 1)
-print(deepestIndeces)
-centers = []
-for i in deepestIndeces:
-    cx, cy = findCenter(contours[i])
-    print(cx, cy)
-    centers.append((cx, cy))
-    cv2.circle(image, (int(cx), int(cy)), 4, (0, 255, 0), 1)
-circularityCutoff = 0.8
-areaMin = 1000
-areaMax = 700000
-circles = []
-for contour in contours:
-    area = cv2.contourArea(contour)
-    perimeter = cv2.arcLength(contour, True)
-    if perimeter != 0:
-        circularity = (4 * math.pi * area) / (perimeter**2)
-        if circularity > circularityCutoff and areaMin < area < areaMax:
-            circles.append(cv2.minEnclosingCircle(contour))
-distanceCutoff = 20
-radiusCutoff = 20
-centerRadiiMap = dict()
-for center in centers:
+contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+centers = findCenters(gray)
+circles = findCircles(contours)
+
+maxRadius = min(image.shape[0] / 2, image.shape[1] / 2)
+
+
+radiiMap = findRadii(centers, circles, maxRadius)
+print(radiiMap)
+averageRadii = None
+radiiCountCutoff = 1
+for center in radiiMap.keys():
     cx, cy = center
-    radiiSums = []
-    radiiCounts = []
-    for circle in circles:
-        (x, y), r = circle
-        if abs(cx - x) < distanceCutoff and abs(cy - y) < distanceCutoff:
-            needToAdd = True
-            for index in range(len(radiiSums)):
-                if abs(radiiSums[index] - r) < radiusCutoff:
-                    radiiSums[index] += r
-                    radiiCounts[index] += 1
-                    needToAdd = False
-                    break
-            if needToAdd:
-                radiiSums.append(r)
-                radiiCounts.append(1)
-    radii = []
-    for index in range(len(radiiCounts)):
-        radii.append(radiiSums[index] / radiiCounts[index])
-    centerRadiiMap[(cx, cy)] = radii
-for center in centerRadiiMap.keys():
-    cx, cy = center
-    radii = centerRadiiMap[center]
+    radii = radiiMap[center]
+    if len(radii) > radiiCountCutoff:
+        if averageRadii is None:
+            averageRadii = np.array(radii)
+        else:
+            averageRadii += np.array(radii)
     for radius in radii:
-        cv2.circle(image, (int(cx), int(cy)), int(radius), (0, 255, 0), 2)
-print(centerRadiiMap)
-print(radii)
-print(cx, cy)
+        cv2.circle(image, (int(cx), int(cy)), int(radius), (0,255,0), 2)
+averageRadii /= len(averageRadii)
+
 cv2.imshow('Contours', image)
-cv2.imshow('edges', edges)
+cv2.imwrite('edges.png', edges)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
